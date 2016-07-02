a5f1301a07cc536f193672c6bc35b26f4313d88a
Kevin Greene <kevindgreene@gmail.com>
2016-07-02 13:05:57 -0400

    Added cut line

--- cards.rb
+++ cards.rb
@@ -2,7 +2,8 @@ require 'squib'
 
 cards = Squib.csv file: 'cards.csv'
 
-Squib::Deck.new(cards: cards['number'].size) do
+Squib::Deck.new(cards: cards['number'].size, layout: "layout.yml") do
+  rect layout: 'cut'
   text str: cards['number'] 
   save format: :pdf
 end

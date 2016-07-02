d9242169e5cf92daf5d518cd743d227c0bafaeff
Kevin Greene <kevindgreene@gmail.com>
2016-07-02 18:16:33 -0400

    Finished part 2

---
+++ Guardfile
@@ -0,0 +1,5 @@
+guard 'rake', :task => 'cards' do
+  watch(%r{^cards.rb})
+  watch(%r{^layout.yml})
+  watch(%r{^cards.csv})
+end
--- cards.rb
+++ cards.rb
@@ -3,8 +3,10 @@ require 'squib'
 cards = Squib.csv file: 'cards.csv'
 
 Squib::Deck.new(cards: cards['number'].size, layout: "layout.yml") do
+  background color: :light_grey
   rect layout: 'cut'
-  text str: cards['number'] 
-  save format: :pdf
+  text str: cards['number'], layout: "number_ul"
+  text str: cards['number'], layout: "number_lr"
+  png file: cards['character'].map { |c|  "assets/#{c}.png"}, layout: cards['character']
+  save format: :pdf, trim: 37.5
 end
-
